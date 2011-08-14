197ec4020a66456b2c7f8c88b8e081ccac055e5e
Manuel Odendahl <wesen@ruinwesen.com>
2011-08-14 15:55:24 +0200

    Add new unit tests

--- tests/testRest.php
+++ tests/testRest.php
@@ -21,7 +21,7 @@ class SimpleHandler {
 
 class SimpleHandler2 {
   /**
-   * @url GET /test/$id
+   * @url GET /test2/$id
    * @noAuth
    **/
   function simpletest($id) {
@@ -31,7 +31,7 @@ class SimpleHandler2 {
 
 class SimpleHandler3 {
   /**
-   * @url GET /test/$par1/$par2
+   * @url GET /test3/$par1/$par2
    * @noAuth
    **/
   function simpletest($par1, $par2) {
@@ -49,13 +49,87 @@ class SimpleHandler3 {
 
 class StaticHandler {
   /**
-   * @url GET /simpletest
+   * @url GET /statictest
    * @noAuth
    **/
   public static function simpletest() {
     return 'static';
   }
 };
+
+class MultipleHandler {
+  /**
+   * @url GET /m1/$par1/$par2
+   * @noAuth
+   **/
+  function m1($par1, $par2) {
+    return array($par1, $par2);
+  }
+
+  /**
+   * @url GET /m1/$par1
+   * @noAuth
+   **/
+  function m1_2($par1) {
+    return $par1;
+  }
+}
+
+class OrderHandler {
+  /**
+   * @url GET /m2/$par1,$par2
+   * @noAuth
+   **/
+  function m2($par1, $par2) {
+    return array($par1, $par2);
+  }
+
+  /**
+   * @url GET /m2/$par1
+   * @noAuth
+   **/
+  function m2_2($par1) {
+    return $par1;
+  }
+
+  /**
+   * @url GET /m2/$par1,$par2,$par3
+   * @noAuth
+   **/
+  function m2_3($par1, $par2, $par3) {
+    return array($par1, $par2, $par3);
+  }
+};
+
+class GetParamsHandler {
+  /**
+   * @url GET /get
+   * @noAuth
+   **/
+  function get_test($__GET) {
+    return $__GET;
+  }
+}
+
+class UrlMatchesHandler {
+   /**
+   * @url GET /foo/$p1/$p2/$p3
+   * @noAuth
+   **/
+  function get_path() {
+    return "path";
+  }
+
+  /**
+   * @url GET /matches/$p1/$p2/$p3
+   * @noAuth
+   **/
+  function get_matches($__requestPath, $__handler, $__urlMatches) {
+    return array("path" => $__requestPath,
+                 "handler" => $__handler,
+                 "matches" => $__urlMatches);
+  }
+}
 
 
 class TestRest extends UnitTestCase {
@@ -90,7 +164,7 @@ class TestRest extends UnitTestCase {
   }
 
   public function testHandleSimple() {
-    $this->server->addClass('SimpleHandler');
+    $this->server->addHandler('SimpleHandler');
     $result = $this->server->handle('simpletest');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
@@ -98,37 +172,37 @@ class TestRest extends UnitTestCase {
   }
 
   public function testHandleStatic() {
-    $this->server->addClass('StaticHandler');
-    $result = $this->server->handle('simpletest');
+    $this->server->addHandler('StaticHandler');
+    $result = $this->server->handle('statictest');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], 'static');
   }
   
   public function testHandleSimpleId() {
-    $this->server->addClass('SimpleHandler2');
-    $result = $this->server->handle('test/1');
+    $this->server->addHandler('SimpleHandler2');
+    $result = $this->server->handle('test2/1');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], 1);
-    $result = $this->server->handle('test/2');
+    $result = $this->server->handle('test2/2');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], 2);
   }
 
   public function testHandleSimpleTwoParams() {
-    $this->server->addClass('SimpleHandler3');
-    $result = $this->server->handle('test/1/2');
+    $this->server->addHandler('SimpleHandler3');
+    $result = $this->server->handle('test3/1/2');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], array(1, 2));
-    $result = $this->server->handle('test/2/3');
+    $result = $this->server->handle('test3/2/3');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], array(2, 3));
 
-      $result = $this->server->handle('inverted_test/1/2');
+    $result = $this->server->handle('inverted_test/1/2');
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], array(1, 2));
@@ -136,17 +210,92 @@ class TestRest extends UnitTestCase {
     $this->assertEqual($result["status"], "200");
     $this->assertFalse($result["error"]);
     $this->assertEqual($result["data"], array(2, 3));
-}
+  }
+
+  public function testMultipleHandlerInitialization() {
+    $this->server = new Rest\Server(array('mode' => 'debug',
+                                          'isCLI' => true,
+                                          'handlers' => array('SimpleHandler3',
+                                                              'SimpleHandler2',
+                                                              'StaticHandler',
+                                                              'SimpleHandler')));
+    $result = $this->server->handle('simpletest');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], 1);
+
+    $result = $this->server->handle('statictest');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], 'static');
+
+    $result = $this->server->handle('test3/1/2');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], array(1, 2));
+    $result = $this->server->handle('test3/2/3');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], array(2, 3));
+
+    $result = $this->server->handle('inverted_test/1/2');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], array(1, 2));
+    $result = $this->server->handle('inverted_test/2/3');
+    $this->assertEqual($result["status"], "200");
+    $this->assertFalse($result["error"]);
+    $this->assertEqual($result["data"], array(2, 3));
+  }
+
+  public function testMultipleHandlers() {
+    $this->server->addHandler('MultipleHandler');
+    $result = $this->server->handle('m1/foo');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result["data"], "foo");
+
+    $result = $this->server->handle('m1/foo/bar');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result["data"], array("foo", "bar"));
+  }
+
+  public function testDefinitionOrder() {
+    $this->server->addHandler('OrderHandler');
+    
+    $result = $this->server->handle('m2/foo');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result["data"], "foo");
+
+    $result = $this->server->handle('m2/foo,bar');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result["data"], array("foo", "bar"));
+
+    $result = $this->server->handle('m2/foo,bar,baz');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result["data"], array("foo,bar", "baz"));
+  }
+
+  public function testGetParams() {
+    $this->server->addHandler('GetParamsHandler');
+
+    $_GET = array("foo" => "bla");
+    $result = $this->server->handle('get');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result['data'], array("foo" => "bla"));
+  }
+
+  public function testUnboundParams() {
+    $this->server->addHandler('UrlMatchesHandler');
+
+    $result = $this->server->handle('foo/bla/blo/bli');
+    $this->assertEqual($result["status"], "200");
+    $this->assertEqual($result['data'], 'path');
+
+    $result = $this->server->handle('matches/bla/blo/bli');
+    $this->assertEqual($result["status"], "200");
+    //    var_dump($result);
+    // XXX
+  }
 }
 
-/* httpMethod, class, name, authorization, static */
-
-/*
- *   public $httpMethod;
-  public $class;
-  public $methodName;
-  public $args;
-  public $needsAuthorization;
-  public $isStatic;
-*/
 ?>
